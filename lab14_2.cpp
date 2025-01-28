15fa4daa7e105bf61ad75a30629f3085cf8f26d5
ZZX4R0 <manisorn0803@gmail.com>
2025-01-28 12:52:54 +0000

    complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,38 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+void inputMatrix(double A[][N]){
+    for(int i = 0;i <N;i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j = 0;j <N;j++){
+            cin >> A[i][j];
+        }
+    }
+}
+
+void showMatrix(const bool B[][N]){
+    for(int i = 0;i <N;i++){
+        for(int j = 0;j <N;j++){
+            if(!B[i][j])
+            cout << 0 << " ";
+            else cout << 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int i = 0;i <N;i++){
+        for(int j = 0;j <N;j++){
+		    
+            if(A[i][j]>=A[i][j-1] && A[i][j]>=A[i][j+1] && A[i][j]>=A[i-1][j] && A[i][j]>=A[i+1][j]){
+				B[i][j]=true;
+			}
+			else B[i][j]=false;
+        }
+    }
+    for(int k=0;k<N;k++) if(true) B[0][k]=false;
+	for(int k=0;k<N;k++) if(true) B[N-1][k]=false;
+	for(int M=0;M<N;M++) if(true) B[M][0]=false;
+	for(int M=0;M<N;M++) if(true) B[M][N-1]=false;
+}
