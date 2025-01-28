042b20b21c2450d6cfa12e8efa9af04b69f44160
ZZX4R0 <manisorn0803@gmail.com>
2025-01-28 12:51:54 +0000

    complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
-}
+	for(int i = 1;i < N; i++){
+        T key = d[i];
+        int j = i - 1;
+        while (j >= 0 && d[j] < key) {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = key;
+        cout << "Pass " << i << ":";
+	    for(int j=0;j<N;j++) cout << d[j] <<" ";
+	    cout << "\n";
+	    }   
+    }
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
